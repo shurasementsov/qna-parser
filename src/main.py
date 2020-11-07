9755498a10663f793c94f5e9e0a36826ef89c0be
Maskin Dmitriy <fffatal0@gmail.com>
2020-11-07 19:27:53 +0300

    change output to json

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-import data_parser as parser
-import csv_writer
+import question_parser as parser
+import json_writer
 
-fileName = 'questions.csv'
+jsonFileName = 'questions.json'
 
-if __name__ == '__main__':
-    # количество первых вопросов для парсинга:
-    questionsCount = 50
+if __name__ == "__main__":
+    # диапазон вопросов для парсинга:
+    startId = 4
+    endId = 50
 
-    questions = parser.parse(questionsCount)
-    csv_writer.writeToFile('questions.csv', questions)
+    for questionId in range(startId, endId):
+        parsedQuestion = parser.getParsedQuestion(questionId)
+        if (parsedQuestion != None):
+            json_writer.writeJSONtoFile(jsonFileName, parsedQuestion)
+
--- src/question_parser.py
+++ src/question_parser.py
@@ -2,9 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # адрес страницы конкретного вопроса Хабр Q&A это ничто иное как "https://qna.habr.com/q/{id}"
-siteUrl = 'https://qna.habr.com/q/'
-startId = '000005'
-endId = '858763'
+QNA_QUESTION_URL = 'https://qna.habr.com/q/'
 
 # заголовки для HTTP
 headers = {
@@ -26,14 +24,12 @@ def parseTags(document):
         try:
             tags.append(i.attrs['data-tagname'])
         except:
-            # иначе возвращает пустотой  элемент
             continue
     return tags
 
 
 def parseDate(document):
     try:
-        # первая дата в документе – дата поста.
         date = document.findAll('time')[0]['datetime']
     except:
         date = 'undefined'
@@ -123,59 +119,26 @@ def parseRespondents(document):
         try:
             respondents.append(i.text.strip().replace('@', '').replace('Автор вопроса', ''))
         except:
-            # иначе возвращает пустотой  элемент
             continue
     return respondents
 
 
 def parseQuestion(questionId, document):
-    # парсинг заголовков
     title = parseTitle(document).strip()
-    # print('Заголовок: {}'.format(title))
 
-    # Если нет заголовка - URL не работает
     if (title == 'undefined'):
         return
 
-    # парсинг тэгов вопроса
     tags = parseTags(document)
-    # print('Тэги: {}'.format(tags))
-
-    # парсинг кол-ва сложности вопроса
     difficulty = parseDifficulty(document).strip()
-    # print('Сложность вопроса: {}'.format(difficulty))
-
-    # парсинг даты, когда задали вопрос
     date = parseDate(document)
-    # print('Дата: {}'.format(date))
-
-    # парсинг кол-ва просмотров
     viewsCount = parseViewCounts(document).strip('views')
-    # print('Количество просмотров: {}'.format(viewsCount))
-
-    # парсинг кол-ва подписавшихся пользователей
     signersCount = parseSignersCount(document).strip()
-    # print('Кол-во подписавшихся пользователей: {}'.format(signersCount))
-
-    # парсинг кол-ва решений
     solutionsCount = parseSolutionsCount(document).strip()
-    # print('Кол-во решений: {}'.format(solutionsCount))
-
-    # парсинг кол-ва ответов
     answersCount = parseAnswersCount(document).strip()
-    # print('Кол-во ответов: {}'.format(answersCount))
-
-    # парсинг Никнейма пользователя, который задал вопрос
     askerNickname = parseAskerNickname(document).strip().replace('@', '')
-    # print('Никнейм задавшего вопрос: {}'.format(askerNickname))
-
-    # парсинг никнеймов отвечающих пользователей
     respondents = parseRespondents(document)
-    # print('Никнеймы отвечающих: {}'.format(respondents))
-
-    # парсинг описания вопроса
     descriptionLength = parseDescriptionLength(document)
-    # print('Описание: {}'.format(descriptionLength))
 
     return {
         'questionId': questionId,
@@ -192,26 +155,9 @@ def parseQuestion(questionId, document):
         'descriptionLength': descriptionLength
     }
 
-    # question = Question(questionId, title, tags, difficulty, date, viewsCount,
-    #          signersCount, solutionsCount, answersCount, askerNickname, respondents, description)
 
-
-def parse(questionsCount):
-    questions = []
-
-    # URI parameter
-    questionId = 5
-
-    while (questionId < questionsCount):
-        page = dowloadPage(siteUrl + str(questionId).zfill(6))  # я хочу создать url вида
-        # ...https://qna.habr.com/q/{число из шести символов}, поэтому такой цикл
-        # zfill() – добавляет незначащие нули к числу
-        # str(i) – преобразование к строке, чтобы приписать незначащие нули
-        document = BeautifulSoup(page.text, "html.parser")
-        question = parseQuestion(questionId, document)
-        questions.append(question)
-
-        # потому что ссылки доступны с нечётным id
-        # (некоторые id всё равно по итогу проваливаются: undefined в выводе)
-        questionId += 2
-    return questions
+def getParsedQuestion(questionId):
+    page = dowloadPage(QNA_QUESTION_URL + str(questionId).zfill(6))
+    document = BeautifulSoup(page.text, "html.parser")
+    question = parseQuestion(questionId, document)
+    return question
